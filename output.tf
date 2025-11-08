4ace88cffea2764d5207a3216e0c3d118df89111
prashant18tech <65825412+prashant18tech@users.noreply.github.com>
2025-11-08 16:02:05 +0530

    Add outputs for resource group name and location
    Added outputs for resource group name and location.

--- output.tf
+++ output.tf
@@ -1,4 +1,8 @@
-output "resource_group_id" {
-  description = "The ID of the Resource Group"
-  value       = azurerm_resource_group.rg.id
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
+
+output "resource_group_location" {
+  value = azurerm_resource_group.rg.location
+}
+
