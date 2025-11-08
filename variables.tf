81f47ede7ba5457872c1a08dfd2d83d73013b97f
prashant18tech <65825412+prashant18tech@users.noreply.github.com>
2025-11-08 16:27:01 +0530

    Refactor Azure variable definitions in variables.tf
    Removed old Azure variable definitions and added new variables for subscription and client details.

--- variables.tf
+++ variables.tf
@@ -1,32 +1,12 @@
-variable "azure_subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
 
-variable "azure_client_id" {
-  description = "Azure Client ID"
-  type        = string
-}
-
-variable "azure_client_secret" {
-  description = "Azure Client Secret"
-  type        = string
-  sensitive   = true
-}
-
-variable "azure_tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "Name of the Resource Group"
-  type        = string
-  default     = "rg-demo-jenkins"
+variable "rg_name" {
+  default = "demo-rg"
 }
 
 variable "location" {
-  description = "Azure region where RG will be created"
-  type        = string
-  default     = "East US"
+  default = "East US"
 }
