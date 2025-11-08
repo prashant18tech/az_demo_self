33e244b8040caf9b5a44f6e818a1858b690214c3
prashant18tech <65825412+prashant18tech@users.noreply.github.com>
2025-11-08 15:54:18 +0530

    Add Azure variables to variables.tf
    Added variables for Azure subscription, client, secret, and tenant.

--- variables.tf
+++ variables.tf
@@ -1,3 +1,25 @@
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "azure_client_id" {
+  description = "Azure Client ID (App ID)"
+  type        = string
+}
+
+variable "azure_client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
