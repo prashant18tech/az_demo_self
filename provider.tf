4551ea01ceee376658cf6903057b46e5340ed709
prashant18tech <65825412+prashant18tech@users.noreply.github.com>
2025-11-08 15:53:30 +0530

    Refactor provider.tf by removing Terraform block
    Removed Terraform block and required providers configuration.

--- provider.tf
+++ provider.tf
@@ -1,14 +1,9 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-
-  required_version = ">= 1.1.0"
-}
-
 provider "azurerm" {
   features {}
+
+  # These environment variables come from Jenkins credentials
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
 }
