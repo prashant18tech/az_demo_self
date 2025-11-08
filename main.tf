51ca96abb90ddba1106be8da8cb0fc6c04ca8246
prashant18tech <65825412+prashant18tech@users.noreply.github.com>
2025-11-08 16:26:24 +0530

    Rename resource group variable for consistency

--- main.tf
+++ main.tf
@@ -1,9 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  required_version = ">=1.4.0"
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+  name     = var.rg_name
   location = var.location
 }
 
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
 
