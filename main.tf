744703dde47eb4d73de33d070e53bef36453af28
prashant18tech <65825412+prashant18tech@users.noreply.github.com>
2025-11-08 15:49:56 +0530

    Change azurerm provider version to ~>3.0
    Updated azurerm provider version to ~>3.0.

--- main.tf
+++ main.tf
@@ -2,18 +2,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.112.0"
+      version = "~>3.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
-  # These environment variables are read automatically from Jenkins:
-  # AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_TENANT_ID, AZURE_SUBSCRIPTION_ID
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
+# Optional: Backend block can go here too
