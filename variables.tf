f94fdaba8b507d7142e0fc612b755b36a152ac77
prashant18tech <65825412+prashant18tech@users.noreply.github.com>
2025-11-08 17:42:11 +0530

    03-Define VNet and Subnet variables in variables.tf
    Added variables for VNet and Subnet configuration

--- variables.tf
+++ variables.tf
@@ -15,3 +15,28 @@ variable "storage_account_name" {
   description = "Name of the storage account"
   type        = string
 }
+
+variable "vnet_name" {
+  description = "Name of the Virtual Network"
+  type        = string
+  default     = "demo-vnet"
+}
+
+variable "vnet_address_space" {
+  description = "Address space for the VNet"
+  type        = list(string)
+  default     = ["192.168.0.0/16"]
+}
+
+variable "subnet_name" {
+  description = "Name of the Subnet"
+  type        = string
+  default     = "demo-subnet"
+}
+
+variable "subnet_prefix" {
+  description = "Subnet prefix"
+  type        = string
+  default     = "192.168.1.0/24"
+}
+
